c202929ab4a3f79e98ad552c11e03edb5ceeceb2
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-09-09 09:43:28 +1000

    first commit- set up

---
+++ index.js
@@ -0,0 +1,14 @@
+const { downloadPokemonPicture } = require("./downloader.js");
+
+downloadPokemonPicture().then(savedFileOutput => {
+	console.log("New image is saved to: " + savedFileOutput);
+}).catch(error => {
+	console.log(error);
+});
+
+//  same function like the above but using async
+async function exampleDownload(){
+	let savedFileOutput = await downloadPokemonPicture();
+	console.log("New image is async saved to: " + savedFileOutput);
+}
+exampleDownload();
